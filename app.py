35b998ba268fec357db3e2059c27f351762921ec
Fav003 <123594397+FcoJavierAV@users.noreply.github.com>
2025-02-06 12:09:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,14 @@ if uploaded_file is not None:
     # Procesar la imágen
     image = Image.open(uploaded_file).convert('L') # Convertir RGB a ByN
     image = image.resize((28, 28))
-    img_array = np.array(image) / 255.0 # Normalizar
-    ''' El primer 1 indica que sólo hay una imágen, luego las dimensiones
-        y el último 1 indica que sólo hay un canal de color.'''
-    img_array = img_array.reshape(1, 28, 28, 1)
+    image = np.array(image).astype('float32') / 255 - 0.5# Normalizar
+    mage = image.reshape(1, 784)
 
     # Motrar la imágen subida
     st.image(image, caption='Imágen subida', use_column_width=True)
 
     # Predicción
-    prediction = model.predict(img_array)
-    classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    st.write(f"Predicción: {classes[np.argmax(prediction)]}")
+    prediction = model.predict(image)
+    predicted_class = np.argmax(prediction)
+    
+    st.image(uploaded_file, caption=f"Predicción: {predicted_class}", use_column_width=True)
